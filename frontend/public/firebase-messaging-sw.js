0458a23656cd837be1c8e4a35e54ef9768ef3598
Areesha Maryam <moezlogics@gmail.com>
2026-06-25 13:35:43 +0500

    feat: add service worker configuration for Firebase messaging push notifications

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+const options = {
+    firebaseConfig: {
+        projectId: "fauji-rashel",
+        messagingSenderId: "619325090904",
+        appId: "1:619325090904:web:c8b342179cc4006d8bae2d",
+        apiKey: "AIzaSyDHr86D_Lljz3rtfUB8YdF7ruCYGojDBtY",
+    },
+    domain: "zmobiles.pk",
+    api_url: "https://push.kolkataff.tv/api/token",
+    vapid_public_key: "BCG82jkDQP5tN1VAatoX4BJLW57Sr0BK8g_gDaOaJt6UnuOlkaIhPKZ9WIHxNxfgOW3CzIJXnfIwGwG7bgjfIn8",
+    http: 0,
+    one_time_collect: 1,
+}
+
+importScripts("https://cdn.larapush.com/sw/larapush-sw-v5.min.js");
